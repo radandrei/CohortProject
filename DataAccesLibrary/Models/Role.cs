5917e65b641b2a09126132657097cfc66f0dda4e
Rad Andrei <rad.andreidan@gmail.com>
2018-03-31 23:12:13 +0300

    Add project files.

---
+++ DataAccesLibrary/Models/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccesLibrary.Models
+{
+    class Role
+    {
+        public int roleID;
+        public string RoleName;
+        public ICollection<User> Users { get; set; }
+    }
+}
---
+++ DataAccesLibrary/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataAccesLibrary.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
