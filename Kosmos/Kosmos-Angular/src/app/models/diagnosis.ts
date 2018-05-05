784a093677cab212eef663e5fc4261e2e6269cad
Rad Andrei <rad.andreidan@gmail.com>
2018-05-05 13:58:56 +0300

    some models

--- Kosmos/Kosmos-Angular/src/app/models/diagnosis.ts
+++ Kosmos/Kosmos-Angular/src/app/models/diagnosis.ts
@@ -3,5 +3,8 @@ import { Prescription } from "./prescription";
 
 export class Diagnosis{
     id:number;
-    prescription:Prescription;
+    name:string;
+    constructor(id:number,name:string){
+        
+    }
 }
